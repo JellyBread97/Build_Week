7f35afa4f5cd09b11e099f05022aa18f053fbeca
annoinspace <aneesahalmask@gmail.com>
2022-10-18 14:45:43 +0100

    bringing first 2 pages together

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// function to move to benchmark quiz from welcome page
+
+function proceed() {
+  let checkbox = document.getElementById("checkbox")
+  let proceed = document.getElementById("button")
+  proceed.addEventListener("click", function () {
+    if (checkbox.checked === false) {
+      alert("You have to promise first!")
+    } else if (checkbox.checked === true) {
+      location.href = "" // benchmark page .html here
+    }
+  })
+}
+
+window.onload = function () {
+  proceed()
+}
+
+// javascript for the benchmark quiz
+
 const questions = [
   {
     question: "How can I create a checkbox in HTML?",
