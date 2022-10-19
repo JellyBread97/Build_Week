9b12f045c58fc6753ebdbd1dddd1a4ff2aac97e1
annoinspace <aneesahalmask@gmail.com>
2022-10-19 10:14:51 +0100

    timer updates
    color change and direction of timer

--- script.js
+++ script.js
@@ -187,79 +187,119 @@ function hideNextButton() {
 startBenchmark()
 
 // code for timer
-const semicircles = document.querySelectorAll(".semicircle")
-const timer = document.querySelector(".timer")
-const secondsDisplay = document.querySelector("#secondsRemaining")
-//input
 
-const hr = 0
-const min = 0
-const sec = 10
+const FULL_DASH_ARRAY = 283
+const WARNING_THRESHOLD = 10
+const ALERT_THRESHOLD = 5
 
-const hours = hr * 3600000
-const minutes = min * 60000
-const seconds = sec * 1000
-const setTime = hours + minutes + seconds
-const startTime = Date.now()
-const futureTime = startTime + setTime
+const COLOR_CODES = {
+  info: {
+    color: "purple"
+  },
+  warning: {
+    color: "turquoise",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+}
 
-const timerLoop = setInterval(countDownTimer)
-countDownTimer()
+const TIME_LIMIT = 15
+let timePassed = 0
+let timeLeft = TIME_LIMIT
+let timerInterval = null
+let remainingPathColor = COLOR_CODES.info.color
 
-function countDownTimer() {
-  const currrentTime = Date.now()
-  const remainingTime = futureTime - currrentTime
-  const angle = (remainingTime / setTime) * 360
-  // progress indicator
-  if (angle > 180) {
-    semicircles[2].style.display = "none"
-    semicircles[0].style.transform = "rotate(-180deg)"
-    semicircles[1].style.transform = `rotate(${angle}deg)`
-  } else {
-    {
-      semicircles[2].style.display = "block"
-      semicircles[0].style.transform = `rotate(${angle}deg)`
-      semicircles[1].style.transform = `rotate(${angle}deg)`
+document.getElementById("timer").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">
+  <span>TIME</span>
+  ${formatTime(timeLeft)}
+  <span>REMAINING</span>
+  </span>
+</div>
+`
+
+startTimer()
+
+function onTimesUp() {
+  clearInterval(timerInterval)
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1
+    timeLeft = TIME_LIMIT - timePassed
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft)
+    setCircleDasharray()
+    setRemainingPathColor(timeLeft)
+
+    if (timeLeft === 0) {
+      onTimesUp()
     }
+  }, 1000)
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60)
+  let seconds = time % 60
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`
   }
 
-  // 5 sec condition
+  return `${seconds}`
+}
 
-  if (remainingTime <= 6000) {
-    semicircles[0].style.backgroundColor = "red"
-    semicircles[1].style.backgroundColor = "red"
-    // semicircles[2].style.display = "block"
-    secondsDisplay.style.color = "red"
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color)
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color)
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color)
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color)
   }
+}
 
-  //timer
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction)
+}
 
-  const hrs = Math.floor(
-    (remainingTime / (1000 * 60 * 60)) % 24
-  ).toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })
-  const mins = Math.floor((remainingTime / (1000 * 60)) % 60).toLocaleString(
-    "en-US",
-    { minimumIntegerDigits: 2, useGrouping: false }
-  )
-  const secs = Math.floor((remainingTime / 1000) % 60).toLocaleString("en-US", {
-    minimumIntegerDigits: 2,
-    useGrouping: false
-  })
-
-  secondsDisplay.innerHTML = `
-      <div>${secs}</div>
-   `
-  //end
-  if (remainingTime < 0) {
-    clearInterval(timerLoop)
-    semicircles[0].style.display = "none"
-    semicircles[1].style.display = "none"
-    semicircles[2].style.display = "none"
-
-    secondsDisplay.innerHTML = `
-    <div>00</div>`
-    secondsDisplay.style.color = "lightgray"
-  }
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray)
 }
 
 function stopTimer() {}
